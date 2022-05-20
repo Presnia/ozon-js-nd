48167f093a7aee8830fcbf0a88445b573f893701
Andrei Dzebysh <presnia@yahoo.com>
2022-05-20 09:06:41 +0300

    Implemented filtering of products by price and sale action

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -7,3 +7,23 @@ export const searchFilter = (goods, value) => {
 export const categoryFilter = (goods, value) => {
   return goods.filter((goodsItem) => goodsItem.category === value);
 };
+
+export const priceFilter = (goods, min, max) => {
+  return goods.filter((goodsItem) => {
+    if (min === '' && max === '') {
+      return goodsItem;
+    } else if (min !== '' && max !== '') {
+      return goodsItem.price > +min && goodsItem.price < +max;
+    } else if (min !== '' && max === '') {
+      return goodsItem.price > +min;
+    } else if (min === '' && max !== '') {
+      return goodsItem.price < +max;
+    }
+  });
+};
+
+export const hotSaleFilter = (goods, value) => {
+  return goods.filter((goodsItem) =>
+    value ? goodsItem.sale === true : goodsItem
+  );
+};
